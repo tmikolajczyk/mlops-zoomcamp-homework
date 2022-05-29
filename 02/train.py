46e1219e74f0eaee222d05ed4b0e98ba4284e152
tom <t.mikolajczyk@gmail.com>
2022-05-29 21:03:58 +0200

    Homework #2

---
+++ 02/train.py
@@ -0,0 +1,46 @@
+import argparse
+import mlflow
+import os
+import pickle
+
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_squared_error
+
+mlflow.set_tracking_uri("http://127.0.0.1:5000")
+mlflow.set_experiment("nyc-taxi-experiment")
+mlflow.autolog()
+
+
+def load_pickle(filename: str):
+    with open(filename, "rb") as f_in:
+        return pickle.load(f_in)
+
+
+def run(data_path):
+
+
+    X_train, y_train = load_pickle(os.path.join(data_path, "train.pkl"))
+    X_valid, y_valid = load_pickle(os.path.join(data_path, "valid.pkl"))
+
+    with mlflow.start_run():
+
+        mlflow.set_tag("developer", "tom")
+        # mlflow.log_param()
+
+        rf = RandomForestRegressor(max_depth=10, random_state=0)
+        rf.fit(X_train, y_train)
+        y_pred = rf.predict(X_valid)
+        rmse = mean_squared_error(y_valid, y_pred, squared=False)
+
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--data_path",
+        default="./output",
+        help="the location where the processed NYC taxi trip data was saved."
+    )
+    args = parser.parse_args()
+
+    run(args.data_path)
